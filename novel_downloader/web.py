9e95cbf2ef620c3324b1b46bb2e0b3dcc6e611c4
winxos <winxos@hotmail.com>
2017-04-04 23:05:02 +0800

    flask async request framework, simplest

--- novel_downloader/web.py
+++ novel_downloader/web.py
@@ -2,16 +2,47 @@
 from novel_grab import novel_grab
 from flask import Flask, request, render_template
 import time
+import threading
 
 app = Flask(__name__)
+
+novels = [  # fake array of posts
+    {
+        'id': 1,
+        'name': 'Beautiful day in Portland!',
+        'from': 'ffff',
+        'state': 0,
+        'download': 'xxx',
+    },
+    {
+        'id': 2,
+        'name': 'aaa day in Portland!',
+        'from': 'ffff2',
+        'state': 100,
+        'download': 'ccc',
+    }
+]
+
+
+def start_thread(url):
+    global novels
+    for i in range(11):
+        novels[0]['state'] = i*10
+        time.sleep(1)
+
+
+cache = {}
 
 
 @app.route('/')
 def index():
+    global cache
     url = request.query_string.decode('utf8')
-    print(url)
-    ct = 10
-    return render_template('index.html', count=ct)
+    if url not in cache:
+        t = threading.Thread(target=start_thread, args=(url,))
+        t.start()
+        cache[url] = 0
+    return render_template('index.html', name=url, novels=novels)
 
 
 if __name__ == '__main__':
